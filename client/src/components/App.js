913999ff2f1958c347c940721589b4d94bc9527c
Michael Reiter <michaelreiter@live.ca>
2016-09-17 08:48:22 -0400

    Update landing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Landing from './Landing';
 import '../styles/App.sass';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Landing />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
 const Landing = () => {
+  // let auth0 = new Auth0({
+  //   domain:         'michaelreiter.auth0.com',
+  //   clientID:       '98aRn2p8ptAZo7acb4Rb6mQYVIwSRh1m',
+  //   callbackURL:    'http://localhost:3000/'
+  // });
+
+  let userRef;
+  let passwordRef;
+
   return (
     <div className='landing'>
+      <h1>BlockBlog</h1>
       <label>Email</label>
-      <input type="text" id="email" />
+      <input type="text" id="email" ref={node => {
+        userRef = node;
+      }}/>
       <label>Password</label>
-      <input type="password" id="password" />
-      <button className="signin-db">Sign in</button>
+      <input type="password" id="password" ref={node => {
+        passwordRef = node;
+      }}/>
+      <button className="signin-db" onClick={() => {
+        const username = userRef.value.trim();
+        const password = passwordRef.value.trim();
+        // auth0.signin({
+        //   connection: 'foo',
+        //   username: userRef,
+        //   password: passwordRef
+        // },
+        // (err, profile, id_token, access_token, state) => {
+        //     // store the profile and id_token in a cookie or local storage
+        //     $.cookie('profile', profile);
+        //     $.cookie('id_token', id_token);
+        // });
+      }}>Sign in</button>
     </div>
   );
 }
