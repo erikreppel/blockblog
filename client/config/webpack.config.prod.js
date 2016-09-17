536e16e6d5711f80154d48ce33096d07babfccb1
Michael Reiter <michaelreiter@live.ca>
2016-09-17 12:29:03 -0400

    Update webpack configs to test splitting bundles (WIP)

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -35,10 +35,16 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: 'source-map',
   // In production, we only want to load the polyfills and the app code.
-  entry: [
-    require.resolve('./polyfills'),
-    path.join(paths.appSrc, 'index')
-  ],
+  entry: {
+    newsfeed: [
+      require.resolve('./polyfills'),
+      path.join(paths.appSrc, 'newsfeed')
+    ],
+    profile: [
+      require.resolve('./polyfills'),
+      path.join(paths.appSrc, 'newsfeed')
+    ]
+  },
   output: {
     // The build folder.
     path: paths.appBuild,
