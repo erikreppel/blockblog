5abe5df39def29c0652af400cc0d60b037c528e4
Erik Reppel <erikreppel@gmail.com>
2016-09-17 10:03:27 -0700

    us redis to cache for now

--- server/data.py
+++ server/data.py
@@ -1,12 +1,14 @@
-def set_data(user_id, post_id, data):
-    pass
+import redis
 
-def get_data(user_id, post_id):    
-    return {
-        "user": "testuser",
-        "user_id": "123",
-        "body": "Hello, World",
-        "timestamp": 92845720,
-        "url": None,
-        "tags": []
-    }
+
+def set_data(user_id, post_id, data):
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    key = user_id + str(post_id)
+    r.set(key, data)
+
+
+def get_data(user_id, post_id):
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)    
+    key = user_id + str(post_id)
+    data = r.get(key)
+    return data
