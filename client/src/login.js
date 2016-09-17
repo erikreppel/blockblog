b71bffb29066e0c84b24d5ef5336d6dffc7c63a9
Michael Reiter <michaelreiter@live.ca>
2016-09-17 12:11:59 -0400

    Split index

---
+++ client/src/login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Landing from './components/Landing';
+import './styles/index.sass';
+
+ReactDOM.render(
+  <Landing />,
+  document.getElementById('root')
+);
---
+++ client/src/newsfeed.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+import './styles/index.sass';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+);
