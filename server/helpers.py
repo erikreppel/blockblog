8948636ef12a624fa21fb5cbed8dda6a4b4b8809
Michael Reiter <michaelreiter@live.ca>
2016-09-18 06:54:26 -0400

    Fix header bug

--- server/helpers.py
+++ server/helpers.py
@@ -2,13 +2,19 @@ import json
 
 
 def load_entity(ent):
-    data = dict(ent)
-    data['following'] = json.loads(data['following'])
-    data['posts'] = json.loads(data['posts'])
-    return data
+    if ent:
+        data = dict(ent)
+        data['following'] = json.loads(data['following'])
+        data['posts'] = json.loads(data['posts'])
+        return data
+    else:
+        return {}
 
 
 def make_entity(data):
-    data['following'] = json.dumps(data['following'])
-    data['posts'] = json.dumps(data['posts'])
-    return data
+    if data:
+        data['following'] = json.dumps(data['following'])
+        data['posts'] = json.dumps(data['posts'])
+        return data
+    else:
+        return {}
