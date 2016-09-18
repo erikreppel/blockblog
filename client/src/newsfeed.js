24c1a7e2aaa5dbc424b947e5fb2d8d443eb52ed5
Michael Reiter <michaelreiter@live.ca>
2016-09-17 21:06:27 -0400

    Update newsfeed wrapper

--- client/src/newsfeed.js
+++ client/src/newsfeed.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home';
+import { getCookie } from './helpers'
 import './styles/index.sass';
 
+const user_id = getCookie('user_id');
+
 ReactDOM.render(
-  <Home />,
+  <Home user_id={user_id}/>,
   document.getElementById('root')
 );
